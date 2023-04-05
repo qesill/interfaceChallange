d9a554bdde796766a17206e031ea61732ad39b6b
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-04-05 22:38:58 +0200

    Poprawki

--- src/Browser.java
+++ src/Browser.java
@@ -1,5 +1,5 @@
 public interface Browser {
-    static String ENGINE = "WebKit";
+    static final String ENGINE = "WebKit";
     public default String getEngine(){return this.ENGINE;}
     abstract void gotoPage(String url);
     abstract void refreshPage();
